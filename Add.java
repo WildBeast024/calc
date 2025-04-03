b80151e72e904fc921551ecf2101283a2819f7b9
Meghraj Shinganjude <106152390+WildBeast024@users.noreply.github.com>
2025-04-03 15:25:40 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 
-public class SumOfTwoNumbers {
+public class Multiplication {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
+        double num1 = scanner.nextDouble();
 
         System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
+        double num2 = scanner.nextDouble();
 
-        int sum = num1 + num2;
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        double product = num1 * num2;
+
+        System.out.println("The multiplication of " + num1 + " and " + num2 + " is: " + product);
 
         scanner.close();
     }
